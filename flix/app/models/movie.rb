123b57acc9e29c1b99203593170fbc03f4a27848
Mario Solomou <webmaster@masodev.com>
2014-06-23 21:53:18 +0300

    Revert "Revert "Custom Queries""
    This reverts commit d5f1f6469f5d3231f0e569c9e7006335310db42b.

--- flix/app/models/movie.rb
+++ flix/app/models/movie.rb
@@ -2,6 +2,11 @@ class Movie < ActiveRecord::Base
 
  def flop?
     total_gross.blank? || total_gross < 50000000
-  end
+ end
 
+  def self.released
+    where("released_on <= ?", Time.now).order("released_on desc")
+
+  end
+  
 end
