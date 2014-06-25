8f3e398665372dc72ae65d7f32558fab405afec6
Mario Solomou <webmaster@masodev.com>
2014-06-25 12:05:58 +0300

    Migrations Revisited Part 2 with Layouts and Assets
    New Design using CSS and SASS.

--- flix/app/controllers/movies_controller.rb
+++ flix/app/controllers/movies_controller.rb
@@ -6,32 +6,37 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
-
-   def edit
+  
+  def edit
     @movie = Movie.find(params[:id])
-  end
-
-  def update
-    @movie = Movie.find(params[:id])
-    @movie.update(params.require(:movie).permit!)  
-    redirect_to @movie
   end
   
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update(movie_params)
+    redirect_to @movie
+  end
+
   def new
     @movie = Movie.new
   end
   
   def create
-  params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross, :cast, :director, :duration, :image_file_name)
-  @movie = Movie.new(movie_params)
-  @movie.save
-  redirect_to @movie
+    @movie = Movie.new(movie_params)
+    @movie.save
+    redirect_to @movie
   end
   
   def destroy
-  @movie = Movie.find(params[:id])
-  @movie.destroy
-  redirect_to movies_url
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_url
   end
-   
+  
+private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross, :cast, :director, :duration, :image_file_name)
+  end
 end
+
